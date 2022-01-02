8fc5668c64a70ad5bd7ab0077d5483294a69f7f4
AjaySinghPanwar <a.panwar48656@gmail.com>
2022-01-03 00:29:25 +0530

    App.js file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Header from './Components/Header';
 import Todo from './Components/Todo';
 import './style.css';
-var todoId = 0;
 
 export default function App() {
     return(
